9c9cd052ab6f7dc45e7d95192c7a48dc5b1f8b00
Akash <aingale354@gmail.com>
2023-04-06 10:24:54 +0530

    Basic_programs

---
+++ src/Basic_programs/Arithmatictest.java
@@ -0,0 +1,9 @@
+package Basic_programs;
+
+public class Arithmatictest {
+    public static void main(String[] args) {
+        int a=20;
+        int b=25;
+        System.out.println(a+b);
+    }
+}
