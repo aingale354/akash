d50b120c49324f9dc5fcc29d2777946841e63a96
aingale354 <130028692+aingale354@users.noreply.github.com>
2023-04-16 23:42:41 +0530

    Multiplication

---
+++ src/Coding/hackerRank.java
@@ -0,0 +1,18 @@
+package Coding;
+
+import java.util.Scanner;
+
+public class hackerRank {
+    static int num=2;
+
+    public static void main(String[] args) {
+
+
+            for(int i = 1; i <= 10; i++) {
+        int result = num * i;
+        System.out.println(num + " x " + i + " = " + result);
+    }
+
+}
+
+}
